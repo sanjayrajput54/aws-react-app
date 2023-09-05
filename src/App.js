cdb5d4e94350970217468c25c0c6e3edcaa85f7c
Sanjay Kumar1 <Sanjay.Kumar1@XEBIAINDIA.LOCAL>
2023-09-05 18:06:22 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="Container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="./logo.jpg" className="App-logo" alt="logo" />
       </header>
+        <h1>AWS React APP</h1>
+      </div>
     </div>
   );
 }
